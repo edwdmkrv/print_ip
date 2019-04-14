0d55fc9661da94435a1823d19e0ee61fdac58ad5
edwdmkrv <46138366+edwdmkrv@users.noreply.github.com>
2019-04-14 04:44:49 +0300

    Revert "Remove files for review" for review
    This reverts commit e0edf5b9add395c224e31d15f3cf13d68075e4f1.

---
+++ src/print_ip.cpp
@@ -0,0 +1,48 @@
+#include <cstdlib>
+
+#include <string>
+#include <vector>
+#include <list>
+#include <tuple>
+#include <iostream>
+
+#include "lib.hpp"
+
+using std::literals::string_literals::operator "" s;
+
+template <typename X>
+class proxy { // Non-owning
+private:
+	X const &x;
+
+public:
+	explicit proxy(X const &x) noexcept: x{x} {
+	}
+
+	friend std::ostream &operator <<(std::ostream &o, proxy<X> const &p) {
+		usr::print_ip(p.x, o);
+		return o;
+	}
+};
+
+int main() try {
+	std::cout.exceptions(std::ostream::badbit | std::ostream::failbit | std::ostream::eofbit);
+
+	std::cout << proxy{char{-1}} << std::endl;
+	std::cout << proxy{short{}} << std::endl;
+	std::cout << proxy{2130706433} << std::endl;
+	std::cout << proxy{8875824491850138409} << std::endl;
+
+	std::cout << proxy{"IP address (std::string)"s} << std::endl;
+	std::cout << proxy{std::vector{'v', 'e', 'c', 't', 'o', 'r'}} << std::endl;
+	std::cout << proxy{std::list{std::numeric_limits<long>::min(), std::numeric_limits<long>::max()}} << std::endl;
+	std::cout << proxy{std::tuple<int, int, int, int, int, int>{-1, 0, 15, 255, 65535, 16777215}} << std::endl;
+
+	return EXIT_SUCCESS;
+} catch (std::exception const &e) {
+	std::cerr << "Error: " << e.what() << std::endl;
+	return EXIT_FAILURE;
+} catch (...) {
+	std::cerr << "Error: " << "unidentified exception" << std::endl;
+	return EXIT_FAILURE;
+}
